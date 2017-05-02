0ffd1f13253a2a1934905b5d12790a9442a85b12
kumar-amiti <kumar@amitisoft.com>
2017-05-02 10:47:33 +0530

    Changes in manage test component
    Changes in manage test component  regarding passing data to object..

--- src/app/hr-dashboard/testmanager/managetest/managetest.component.ts
+++ src/app/hr-dashboard/testmanager/managetest/managetest.component.ts
@@ -21,48 +21,8 @@ export class ManagetestComponent implements OnInit {
 
     ngOnInit() {
 
-
-        this.sampleManageTestData.push({
-            id: 1,
-            studentId: 1,
-            name: 'test user',
-            postApplied: 'java developer',
-            emailId: 'test@test.com',
-            date: '24-4-2017',
-            subject: 'java',
-            paper: 'core java',
-            status: 'test status',
-            resendLink: 'www.test.com'
-        });
-        this.sampleManageTestData.push({
-            id: 2,
-            studentId: 2,
-            name: 'test user',
-            postApplied: 'web developer',
-            emailId: 'test1@test.com',
-            date: '24-4-2017',
-            subject: 'html',
-            paper: 'web',
-            status: 'test status',
-            resendLink: 'www.test.com'
-        });
-
-     /** this.mngTestService.getManageTestData()
-          .subscribe(
-          //for loopping // data => this.items = data
-          data => {
-              const myArray = [];
-              for (let key in data) {
-                  myArray.push(data[key]);
-              }
-              this.manageTest = myArray;
-          }
-          ); **/
-
      this.mngTestService.getData()
        .subscribe((data: any) => {
-           console.log(data.bookings[0].candidateFullName)
-
            this.manageTest = data.bookings;
          }
 
@@ -73,25 +33,24 @@ export class ManagetestComponent implements OnInit {
     startTest(sampleManageTestData) {
         this.startTestRows = [];
         for (let i = 0; i < sampleManageTestData.length; i++) {
-            this.rowValue = sampleManageTestData[i].id;
-            var element = <HTMLInputElement>document.getElementById("row" + sampleManageTestData[i].id);
+            this.rowValue = sampleManageTestData[i].candidateId;
+            var element = <HTMLInputElement>document.getElementById("row" + sampleManageTestData[i].candidateId);
             var isChecked = element.checked;
             if (isChecked) {
                 this.startTestRows.push({
-                    id: sampleManageTestData[i].id,
-                    studentId: sampleManageTestData[i].studentId,
-                    name: sampleManageTestData[i].name,
+                    id: sampleManageTestData[i].candidateId,
+                    studentId: sampleManageTestData[i].candidateId,
+                    name: sampleManageTestData[i].candidateFullName,
                     postApplied: sampleManageTestData[i].postApplied,
-                    emailId: sampleManageTestData[i].emailId,
-                    date: sampleManageTestData[i].date,
-                    subject: sampleManageTestData[i].subject,
-                    paper: sampleManageTestData[i].paper,
-                    status: sampleManageTestData[i].status,
+                    emailId: sampleManageTestData[i].candidateMailId,
+                    date: sampleManageTestData[i].DOE,
+                    subject: sampleManageTestData[i].category,
+                    paper: sampleManageTestData[i].paperType,
+                    status: sampleManageTestData[i].testStatus,
                     resendLink: sampleManageTestData[i].resendLink
                 })
                 this.selectedRows = true;
             }
-
         }
 
         if (this.selectedRows) {
