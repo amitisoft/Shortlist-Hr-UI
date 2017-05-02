e3424cc101d35843910499c41ae87c9dd40ecf49
SARATHMC20 <sarathmc2011@gmail.com>
2017-05-02 12:02:30 +0530

    HR- UI

--- src/app/hr-dashboard/testmanager/createtest/createtest.properties.ts
+++ src/app/hr-dashboard/testmanager/createtest/createtest.properties.ts
@@ -4,7 +4,10 @@ export class CreatetestProperties {
 
     constructor() { }
 
-    sendEmailUrl = 'https://questiontable-630db.firebaseio.com/saveSelectedEmailList.json';
+    sendEmailUrl = ' https://amitionlinesendlink.firebaseio.com/saveSelectedEmailList.json';
+
+    //sendEmailUrl = 'https://questiontable-630db.firebaseio.com/saveSelectedEmailList.json';
+
 
     //There is no UI to submit(emails will come from student registration) emails,created json file with bunch of emails and fetching them for auto complete
     getEmailUrl = 'https://questiontable-630db.firebaseio.com/createEmailList.json';
