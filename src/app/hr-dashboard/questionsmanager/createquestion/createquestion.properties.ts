e76812d63a1779ff466833f581e02aa495fd5e35
sharathcb-amiti <sharath.cb@amitisoft.com>
2017-05-09 13:54:22 +0530

    New Api
    New Api

--- src/app/hr-dashboard/questionsmanager/createquestion/createquestion.properties.ts
+++ src/app/hr-dashboard/questionsmanager/createquestion/createquestion.properties.ts
@@ -4,7 +4,8 @@ export class CreateQuestionProperties {
 
     constructor() { }
 
-    saveQuestionUrl = 'https://e92rcpg85i.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
+    saveQuestionUrl = 'https://f8ahuuoen7.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
+    
 
     // saveQuestionUrl = 'https://questiontable-630db.firebaseio.com/createquestionoptions.json';
 
