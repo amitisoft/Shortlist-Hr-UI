c7e0f389fe91af5093e947d0e9ca9917526d3a49
kumar-amiti <kumar@amitisoft.com>
2017-05-09 14:04:53 +0530

    Changes in create test and manage test components
    1.Chnaged api for create test link
    2.chnaged api , object and html in manage test component..

--- src/app/hr-dashboard/testmanager/managetest/managetest.properties.ts
+++ src/app/hr-dashboard/testmanager/managetest/managetest.properties.ts
@@ -5,9 +5,10 @@ export class ManageTestProperties {
     // Getting Manage Test Data
 
     //getManageTestDatas = 'https://api.myjson.com/bins/mm34p';
-    getManageTestDatas = 'https://e92rcpg85i.execute-api.us-east-1.amazonaws.com/dev/api/starttestdashboard/candidateId/4/bookingId/4/testStatus/test%20not%20taken';
+    getManageTestDatas = 'https://f8ahuuoen7.execute-api.us-east-1.amazonaws.com/dev/api/starttestdashboard/candidateId/%7BcandidateId%7D/bookingId/%7BbookingId%7D/testStatus/%7BtestStatus%7D';
 
     // Test Started => Send
 
-    sendTestStartedDatas = 'https://amitionlinemanagetest.firebaseio.com/startTestData.json';
+    //sendTestStartedDatas = 'https://amitionlinemanagetest.firebaseio.com/startTestData.json';
+    sendTestStartedDatas = ' https://f8ahuuoen7.execute-api.us-east-1.amazonaws.com/dev/api/updateBookingAfterStartTest';
 }
