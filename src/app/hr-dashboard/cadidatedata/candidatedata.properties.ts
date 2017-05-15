092932c15d67b5222e08de259d72589b5a82ecd5
sharathcb-amiti <sharath.cb@amitisoft.com>
2017-05-15 11:09:53 +0530

    Removed candidatedata property file.

--- src/app/hr-dashboard/cadidatedata/candidatedata.properties.ts
+++
@@ -1,7 +0,0 @@
-﻿
-
-export class CandidateDataProperties {
-
-    //uploadCandidateDataUrl = 'https://amitionlinetest.firebaseio.com/candidateData.json';
-    uploadCandidateDataUrl = 'https://cwnlvat845.execute-api.us-east-1.amazonaws.com/dev/api/getCandidatesListFile';
-}
