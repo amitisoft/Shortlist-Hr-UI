657b812505a2a3d048ea7c7da670df34c521a31f
satya-amiti <satya@amitisoft.com>
2017-05-15 12:33:43 +0530

    Chnages

--- src/app/hr-dashboard/cadidatedata/candidatedata.properties.ts
+++ src/app/hr-dashboard/cadidatedata/candidatedata.properties.ts
@@ -2,6 +2,6 @@
 
 export class CandidateDataProperties {
 
-    //uploadCandidateDataUrl = 'https://amitionlinetest.firebaseio.com/candidateData.json';
+   // uploadCandidateDataUrl = 'https://amitionlinetest.firebaseio.com/candidateData.json';
     uploadCandidateDataUrl = 'https://cwnlvat845.execute-api.us-east-1.amazonaws.com/dev/api/getCandidatesListFile';
 }
