f3743a58a900b0921b32b48bfa9e24add954319c
satya-amiti <satya@amitisoft.com>
2017-05-22 12:20:23 +0530

    change properties

--- src/app/common.properties.ts
+++ src/app/common.properties.ts
@@ -1,13 +1,13 @@
-﻿export class CommonProperties {
+export class CommonProperties {
 
 
     /*----cadidatedata Component----*/
     uploadCandidateDataUrl = 'https://lgtqza61fa.execute-api.us-east-1.amazonaws.com/dev/api/getCandidatesListFile';
 
    
-    addcandidateurl='https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/insertCandidate' ;//'https://user-a1ecd.firebaseio.com/userlist.json';
-    getcandidateurl='https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/allcandidates';
-    getcandidateDetails='https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/getCandidateInfoForView/candidateId';
+    addcandidateurl='https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/insertCandidate';//'https://user-a1ecd.firebaseio.com/userlist.json';
+    getcandidateurl='https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/allcandidates';
+    getcandidateDetails='https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/getCandidateInfoForView/candidateId';
 
 	/*----categorymanager Component----*/
 
@@ -16,25 +16,25 @@
     getcategoryDetails='https://api.myjson.com/bins/eg9l1';
 
     /*----createquestion Component----*/
-    saveQuestionUrl = 'https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
+    saveQuestionUrl = 'https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
 
     /*----papermanagement Component----*/
     categoryPermission = 'https://amitionlinetestcategory.firebaseio.com/categoryPermission.json';
-    questionpaper = 'https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/questionpaper';
+    questionpaper = 'https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/questionpaper';
 
 
     /*----viewquestions Component----*/
-    viewQuestionsUrl = 'https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/getquestionpapernames';
+    viewQuestionsUrl = 'https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/getquestionpapernames';
 
     /*----createtest Component----*/
-    sendEmailUrl = 'https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/testlink';
+    sendEmailUrl = 'https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/testlink';
     getEmailUrl = 'https://questiontable-630db.firebaseio.com/createEmailList.json';
 
     /*----managetest Component----*/
-    getManageTestDatas = 'https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/starttestdashboard/candidateId/{candidateId}/bookingId/{bookingId}/testStatus/{testStatus}';
+    getManageTestDatas = 'https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/starttestdashboard/candidateId/{candidateId}/bookingId/{bookingId}/testStatus/{testStatus}';
     
     //sendTestStartedDatas = 'https://amitionlinemanagetest.firebaseio.com/startTestData.json';
-    sendTestStartedDatas = 'https://38klsv9mz0.execute-api.us-east-1.amazonaws.com/dev/api/updateBookingAfterStartTest';
+    sendTestStartedDatas = 'https://6x2a0jjalb.execute-api.us-east-1.amazonaws.com/dev/api/updateBookingAfterStartTest';
     // Getting Manage Test Data [Test In Progress]
     getManageTestInProgress = 'https://lgtqza61fa.execute-api.us-east-1.amazonaws.com/dev/api/startTestInProgressDashboard';
 
