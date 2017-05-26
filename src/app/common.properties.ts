361229e3196413d4e0d9d88f7a00b13c0adaa00f
SARATHMC20 <sarathmc2011@gmail.com>
2017-05-26 15:29:30 +0530

    Properties

--- src/app/common.properties.ts
+++ src/app/common.properties.ts
@@ -1,4 +1,4 @@
-export class CommonProperties {
+﻿export class CommonProperties {
 
 
     /*----cadidatedata Component----*/
@@ -17,7 +17,8 @@ export class CommonProperties {
     deletecategories='https://f4l096njvh.execute-api.us-east-1.amazonaws.com/dev/api/deleteCategory/categoryId';
 
     /*----createquestion Component----*/
-    saveQuestionUrl = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
+    saveQuestionUrl = 'https://e6zf4dv2g6.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
+    //saveQuestionUrl = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
 
     /*----papermanagement Component----*/
     categoryPermission = 'https://amitionlinetestcategory.firebaseio.com/categoryPermission.json';
