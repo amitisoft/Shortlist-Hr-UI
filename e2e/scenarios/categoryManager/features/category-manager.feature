e57251e3b8453908cadc9007288236e9e32e6273
prashanth-amiti <prashanth@amitisoft.com>
2017-05-31 16:44:55 +0530

    again added test manager and category manager module and read data from external json file

--- e2e/scenarios/categoryManager/features/category-manager.feature
+++ e2e/scenarios/categoryManager/features/category-manager.feature
@@ -1,76 +1,64 @@
-Feature: HR can view category manager module
+Feature:  HR can create and edit category manager dashboard
+          As a user of Candidate Shortlisting Application
+          I should be able to use category Manager Dashboard
+          In order to Create/Edit and Mange Category
+
   @CreateCategory
 	Scenario: HR can create category in category manager
 
-	Given I am on the Dashboard page
-		When I click on category manager module
-    When verify create category button enabled
-    When  click on create new category
-    When verify add category button enabled
-    When verify clear category button enabled
-		When  enter text  for category name
-    When  enter description for category description
-		When  click on add button
-		#Then  click on clear button
-    When I click ok for alert
-    Then  click on clear button
-    #Then  I should get message as sucessfully created category
+	  Given I am on the Dashboard page
+		When  I click on category manager module
+    And   verify create category button enabled
+    And   click on create new category
+    And   verify add category button enabled
+    And   verify clear category button enabled
+    And   enter text  for category name
+    And   enter description for category description
+    And   click on add button
+    And   verify alert msg and i accept the alert
 
 
+  @EditCategory
+  Scenario: HR can edit view category in category manager
 
+    Given I am the Dashboard
+    When  I click on categorya manager
+    And   click on edit link Button
+    Then  It redirects to page with pre-filled data and verify the data
+    And   Update the data in field
+    And   click on update button
+    And   I click accept the alert
+    And   verify the pre filled data
 
+  @VerifyClearCategory
+  Scenario: Verify clear button category in category manager
 
+	  Given I am on the Dashboard page
+	  When  I click on category manager module
+	  And   I click on create new category
+    And   I enter text  for category name
+    And   I enter description for category description
+	  Then  I click on clear button
+	  And   I verify fields should be empty
+    And   click clear button
 
+  @VerifyCategoryTable
+  #Scenario: HR can view category in category manager
 
+    #Given click on category manager module
+	  #When  I click on view category button
+	  #Then  Verify Hr can able to view the category list
 
+  @VerifyCategoryUpdated##Tab
+  #Scenario: After creating a category it has to updated in question manager
 
-#Feature: HR can view category manager module
-
-# Scenario: HR can create category in category manager
-
-
-
-
-#Scenario: HR cannot create category in category manager by missing fields
-
-#	Given I am on the Dashboard page
-#	When  I click on category manager module
-	#And   click on create new category
-	#And   click on save button
-#	Then  I should get message as please enter mandatory fields
-
-#Scenario: HR can view category in category manager
-
-	#Given click on category manager module
-	#When  I click on view category button
-	#Then  Verify Hr can able to view the category list
-
-@EditCategory
-Scenario: HR can edit view category in category manager
-
-	Given I am on the Dashboard
-	When  I click on categorya manager module
-#	And   click on view category button
-	When   click on edit link Button
-	Then  It redirects to page with pre-filled data and verify the data
-  Then Update the data in field
-  #And   verify the pre filled data
-	And   click on update button
-  And I click accept the alert
-  And   verify the pre filled data
-
-
-
-
-#Scenario: After creating a category it has to updated in question manager
-
-	#Given I am on the Dashboard page
-	#When  I click on category manager module
-	#And   click on create new category
-	#And   enter text  for category name
-	#And   enter description for category description
-	#And   click on save button
-	#When  I click on question mangager module
-	#And   I click on show question
-	#Then  The created category has to be updated succesfully in dropdown
-	#And   Select category from the dropdown
+    #Given I am on the Dashboard page
+	  #When  I click on category manager module
+	  #And   click on create new category
+	  #And   enter text  for category name
+	  #And   enter description for category description
+	  #And   click on save button
+	  #When  I click on question mangager module
+	  #And   I click on show question
+	  #Then  The created category has to be updated succesfully in dropdown
+	  #And   Select category from the dropdown
--- e2e/scenarios/categoryManager/tests/category-manager.e2e-spec.ts
+++ e2e/scenarios/categoryManager/tests/category-manager.e2e-spec.ts
@@ -3,113 +3,114 @@ import {CategoryManagerPage} from "../../../pages/categoryManager/category-manag
 const {defineSupportCode} = require('cucumber');
 import {expect} from 'chai';
 import {TableDefinition} from "cucumber";
-
 import { $ } from 'protractor';
 import { browser,protractor } from 'protractor';
+import * as data from 'C:\\Users\\User\\Desktop\\example.json';
+
 
 
 defineSupportCode(function ({Given,When,Then}) {
 
-    let categorymanagerpage : CategoryManagerPage = new CategoryManagerPage();
-
-  Given('I am on the Dashboard page', () => {
-    return categorymanagerpage.waitHrDashboard();
-  });
-
-  When(/^I click on category manager module$/, () => {
-    return categorymanagerpage.clickCategoryManagerTab();
-  });
-
-  When(/^verify create category button enabled$/, () => {
-    return categorymanagerpage.verifyCreateCategorybtnEnable().then((text)=>{
-      console.log(text);
-    });
-  });
-
-  When(/^click on create new category$/, () => {
-    return categorymanagerpage.clickCreateCategoryManagerTab();
-  });
-
-  When(/^verify add category button enabled$/, () => {
-    return categorymanagerpage.verifyAddCategorybtnEnable().then((text)=>{
-      console.log(text);
-    });
-  });
-  When(/^verify clear category button enabled$/, () => {
-    return categorymanagerpage.verifyClearCategorybtnEnable().then((text)=>{
-      console.log(text);
-    });
-  });
-
-  When(/^enter text  for category name$/, () => {
-    browser.sleep(1000);
-    return categorymanagerpage.enterCategory("mango ");
-  });
-
-  When(/^enter description for category description$/, () => {
-    browser.sleep(1000);
-    return categorymanagerpage.enterCategoryDescription("orange");
-  });
-
-  When(/^click on add button$/, () => {
-    browser.sleep(10000);
-    return categorymanagerpage.clickAddButton();
+    let categorymanagerpage: CategoryManagerPage = new CategoryManagerPage();
+    let utilities: Utilities = new Utilities();
+    Given('I am on the Dashboard page', () => {
+      return categorymanagerpage.waitHrDashboard();
     });
 
-
-
-
-  When(/^I click ok for alert/, () => {
-browser.sleep(20000);
-    return browser.switchTo().alert().accept();
-    //return categorymanagerpage.clickClearButton;
-  });
-  Then(/^click on clear button$/, () => {
-   // browser.pause();
-     return browser.sleep(30000);
-    // return browser.pause();
-   // return categorymanagerpage.clickClearButton;
-  });
-
-/*
-
-
-
-        //return expect(browser.getTitle()).to.eventually.equal('AmitiOnlineTest');
-
-
-
-
-
-    Then(/^Verify Hr can able to view the category list$/, () => {
-        browser.sleep(20000);
-        let headers = element.all(by.xpath('//table//tr//td[1]')).map(function(elm) {
-            return elm.getText();
-        });
-
-        expect(headers).to.equal([
-            "Java",
-            "JavaScript",
-            "QA",
-            "Angular 4",
-            "AWS Engineer",
-            "Bootstrap",
-
-
-        ]);
+    When(/^I click on category manager module$/, () => {
+      return categorymanagerpage.clickCategoryManagerTab();
     });
 
-*/
+    When(/^verify create category button enabled$/, () => {
+      return categorymanagerpage.verifyCreateCategorybtnEnable().then((text) => {
+        console.log(text);
+      });
+    });
 
-})
+    When(/^click on create new category$/, () => {
+      return categorymanagerpage.clickCreateCategoryManagerTab();
+    });
+
+    When(/^verify add category button enabled$/, () => {
+      return categorymanagerpage.verifyAddCategorybtnEnable().then((text) => {
+        console.log(text);
+      });
+    });
+    When(/^verify clear category button enabled$/, () => {
+      return categorymanagerpage.verifyClearCategorybtnEnable().then((text) => {
+        console.log(text);
+      });
+    });
+
+    When(/^enter text  for category name$/, () => {
+      browser.sleep(1000);
+     return categorymanagerpage.enterCategory((<any>data).name);
+
+    });
+
+    When(/^enter description for category description$/, () => {
+      browser.sleep(1000);
+      return categorymanagerpage.enterCategoryDescription("orange");
+    });
+
+    When(/^click on add button$/, () => {
+      browser.sleep(10000);
+      return categorymanagerpage.clickAddButton();
+    });
+
+
+    When(/^verify alert msg and i accept the alert/, () => {
+      browser.sleep(20000);
+
+     return utilities.verifyAlertText("data submitted auccessfully").then((alert)=>{
+        console.log(alert);
+       utilities.acceptAlert();
+      });
+
+    });
+    Then(/^click on clear button$/, () => {
+      return browser.sleep(30000);
+    });
+
+    /*
+
+
+
+     //return expect(browser.getTitle()).to.eventually.equal('AmitiOnlineTest');
+
+
+
+
+
+     Then(/^Verify Hr can able to view the category list$/, () => {
+     browser.sleep(20000);
+     let headers = element.all(by.xpath('//table//tr//td[1]')).map(function(elm) {
+     return elm.getText();
+     });
+
+     expect(headers).to.equal([
+     "Java",
+     "JavaScript",
+     "QA",
+     "Angular 4",
+     "AWS Engineer",
+     "Bootstrap",
+
+
+     ]);
+     });
+
+     */
+  })
+
 defineSupportCode(function ({Given,When,Then}) {
 
   let categorymanageredit: CategoryManagerPage = new CategoryManagerPage();
-
-  Given('I am on the Dashboard', () => {
+  let utilities: Utilities = new Utilities();
+  Given('I am the Dashboard', () => {
     return categorymanageredit.waitHrDashboard();
   });
-  When(/^I click on categorya manager module$/, () => {
+  When(/^I click on categorya manager$/, () => {
     return categorymanageredit.clickCategoryManagerTab();
   });
 
@@ -118,17 +119,16 @@ defineSupportCode(function ({Given,When,Then}) {
     return categorymanageredit.clickEditCategory();
   });
 
-  When(/^It redirects to page with pre-filled data and verify the data$/, () => {
-
+  Then(/^It redirects to page with pre-filled data and verify the data$/, () => {
     browser.sleep(30000);
-        categorymanageredit.verifyTextEditCategory().then((text)=>{
-         console.log(text);
-           expect(text).to.equal('app');
-          browser.sleep(2000);
-          categorymanageredit.verifyTextEditDescCategory().then((text)=>{
-            console.log(text);
-           return expect(text).to.equal('prashanth')
-          });
+    categorymanageredit.verifyTextEditCategory().then((text) => {
+      console.log(text);
+      expect(text).to.equal('testing');
+      browser.sleep(2000);
+      categorymanageredit.verifyTextEditDescCategory().then((text) => {
+        console.log(text);
+        return expect(text).to.equal('orange')
+      });
 
     });
 
@@ -150,14 +150,61 @@ defineSupportCode(function ({Given,When,Then}) {
   Then(/^I click accept the alert$/, () => {
 
     browser.sleep(20000);
-    return browser.switchTo().alert().accept();
+    return utilities.verifyAlertText("data submitted auccessfully").then((alert)=> {
+      console.log(alert);
+      utilities.acceptAlert();
+    });
   });
 
   Then(/^verify the pre filled data$/, () => {
-    browser.pause();
-     browser.sleep(1000);
-     return browser.pause();
-    //browser.sleep(20000);
-   // return browser.switchTo().alert().accept();
+
+    browser.sleep(20000);
   });
 })
+
+defineSupportCode(function ({Given,When,Then}) {
+
+  let categorymanagerclear: CategoryManagerPage = new CategoryManagerPage();
+  Given('I am on the Dashboard', () => {
+    return categorymanagerclear.waitHrDashboard();
+  });
+  When(/^I click on categorya manager module$/, () => {
+    return categorymanagerclear.clickCategoryManagerTab();
+  });
+  When(/^I click on create new category$/, () => {
+    return categorymanagerclear.clickCreateCategoryManagerTab();
+  });
+  When(/^I enter text  for category name$/, () => {
+    browser.sleep(1000);
+    return categorymanagerclear.enterCategory("name");
+  });
+
+  When(/^I enter description for category description$/, () => {
+    browser.sleep(1000);
+    return categorymanagerclear.enterCategoryDescription("orange");
+  });
+  Then(/^I click on clear button$/, () => {
+    browser.sleep(3000);
+    return categorymanagerclear.clickClearButton();
+  });
+  Then(/^I verify fields should be empty$/, () => {
+    browser.sleep(3000);
+    categorymanagerclear.verifyTextEditCategory().then((box) => {
+      console.log(box);
+      expect(box).to.equal(null);
+      browser.sleep(2000);
+      categorymanagerclear.verifyTextEditDescCategory().then((field) => {
+        console.log(field);
+        return expect(field).to.equal(null)
+    });
+  });
+})
+  Then(/^click clear button$/, () => {
+
+    return browser.sleep(3000);
+
+  });
+})
+
+
+//})
