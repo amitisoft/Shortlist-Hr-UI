9541070718e74be4e213e531e399e36493bf30d8
kirankumar-amiti <kiran@amitisoft.com>
2017-05-31 12:09:52 +0530

    updated urls

--- src/app/common.properties.ts
+++ src/app/common.properties.ts
@@ -5,47 +5,47 @@
     uploadCandidateDataUrl = 'https://lgtqza61fa.execute-api.us-east-1.amazonaws.com/dev/api/getCandidatesListFile';
 
 
-    addcandidateurl = 'https://b0ovi53g4m.execute-api.us-east-1.amazonaws.com/dev/api/insertCandidate';//'https://user-a1ecd.firebaseio.com/userlist.json';
-    getcandidateurl = 'https://u4m8g9nxgi.execute-api.us-east-1.amazonaws.com/dev/api/getESCandidateInformation';
-    getcandidateDetails = 'https://b0ovi53g4m.execute-api.us-east-1.amazonaws.com/dev/api/getCandidateInfoForView/candidateId';
-    getcandidatraftersearch = 'https://u4m8g9nxgi.execute-api.us-east-1.amazonaws.com/dev/api/findESCandidateSearchResult';
+    addcandidateurl = 'https://yufinedfhg.execute-api.us-east-1.amazonaws.com/dev/api/insertCandidate';//'https://user-a1ecd.firebaseio.com/userlist.json';
+    getcandidateurl = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/getESCandidateInformation';
+    getcandidateDetails = 'https://yufinedfhg.execute-api.us-east-1.amazonaws.com/dev/api/getCandidateInfoForView/candidateId';
+    getcandidatraftersearch = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/findESCandidateSearchResult';
 
 	/*----categorymanager Component----*/
 
-    sendCategories = 'https://vxouh67191.execute-api.us-east-1.amazonaws.com/dev/api/createCategory';
-    viewCategories = 'https://vxouh67191.execute-api.us-east-1.amazonaws.com/dev/api/getAllCategories';
-    getcategoryDetails = 'https://vxouh67191.execute-api.us-east-1.amazonaws.com/dev/api/getCategoryById';
+    sendCategories = 'https://yufinedfhg.execute-api.us-east-1.amazonaws.com/dev/api/createCategory';
+    viewCategories = 'https://yufinedfhg.execute-api.us-east-1.amazonaws.com/dev/api/getAllCategories';
+    getcategoryDetails = 'https://yufinedfhg.execute-api.us-east-1.amazonaws.com/dev/api/getCategoryById';
     deletecategories = 'https://f4l096njvh.execute-api.us-east-1.amazonaws.com/dev/api/deleteCategory/categoryId';
 
     /*----createquestion Component----*/
-    saveQuestionUrl = 'https://vxouh67191.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
+    saveQuestionUrl = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
    // saveQuestionUrl = 'https://e6zf4dv2g6.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
     //saveQuestionUrl = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/createquestion';
 
     /*----papermanagement Component----*/
     categoryPermission = 'https://amitionlinetestcategory.firebaseio.com/categoryPermission.json';
-    questionpaper = 'https://vxouh67191.execute-api.us-east-1.amazonaws.com/dev/api/questionpaper';
+    questionpaper = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/questionpaper';
    // questionpaper = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/questionpaper';
 
 
     /*----viewquestions Component----*/
-    viewQuestionsUrl = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/getquestionpapernames';
-    getCategoryQuestionsUrl = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/getquestionpapernamesbycategory';
+    viewQuestionsUrl = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/getquestionpapernames';
+    getCategoryQuestionsUrl = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/getquestionpapernamesbycategory';
 
     /*----createtest Component----*/
     sendEmailUrl = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/testlink';
     getEmailUrl = 'https://questiontable-630db.firebaseio.com/createEmailList.json';
 
     /*----managetest Component----*/
-    getManageTestDatas = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/starttestdashboard/candidateId/{candidateId}/bookingId/{bookingId}/testStatus/{testStatus}';
-    postResultManageDatas = ' https://mi07qzvi10.execute-api.us-east-1.amazonaws.com/dev/api/findESResultSearch ';
+    getManageTestDatas = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/getESTestNotTakenResults';
+    postResultManageDatas = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/findESResultSearch';
     //sendTestStartedDatas = 'https://amitionlinemanagetest.firebaseio.com/startTestData.json';
-    sendTestStartedDatas = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/updateBookingAfterStartTest';
-    sendTestStartedDatassearch = 'https://szlqcpr19k.execute-api.us-east-1.amazonaws.com/dev/api/findESBookingSearchResult';
+    sendTestStartedDatas = 'https://yufinedfhg.execute-api.us-east-1.amazonaws.com/dev/api/updateBookingAfterStartTest';
+    sendTestStartedDatassearch = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/findESBookingSearchResult';
 
     /** Result Manager**/
 
-    getResultManagerFullListDataUrl = 'https://mi07qzvi10.execute-api.us-east-1.amazonaws.com/dev/api/getESResultSearch';
+    getResultManagerFullListDataUrl = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/getESResultSearch';
 
     /** Result Manager**/
     // Getting Manage Test Data [Test In Progress]
@@ -54,7 +54,7 @@
     notificationUrl = 'http://localhost:3000/notificationData';
     
 
-    getManageTestInProgress = 'https://8i9879447i.execute-api.us-east-1.amazonaws.com/dev/api/startTestInProgressDashboard';
+    getManageTestInProgress = 'https://yz74ispp1i.execute-api.us-east-1.amazonaws.com/dev/api/getESTestInProgressResults';
 
     //change and forgot password
     changepassword='https://api.myjson.com/bins/eg9l1';
